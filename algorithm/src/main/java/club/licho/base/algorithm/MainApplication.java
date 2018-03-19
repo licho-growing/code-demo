a2c3a4456ddc4e34cfd47dce49738ed6988a9e79
master-666 <17770030629@163.com>
2018-03-19 22:04:33 +0800

    test account

---
+++ algorithm/src/main/java/club/licho/base/algorithm/MainApplication.java
@@ -0,0 +1,13 @@
+package club.licho.base.algorithm;
+
+/**
+ * ClassName:MainApplication
+ *
+ * @author licho
+ * @create 2018-03-19 22:02
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.printf("Hello world");
+    }
+}
