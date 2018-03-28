434134f3ea2ff60a9b87dd372c09985c5d98d9a2
Master-LCL <17770030629@163.com>
2018-03-29 00:28:18 +0800

    1.添加了随机数生成器。

---
+++ algorithm/src/main/java/club/licho/base/algorithm/ArraysGenerator.java
@@ -0,0 +1,50 @@
+package club.licho.base.algorithm;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.jdom.output.DOMOutputter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 数组自动生层器
+ */
+public class ArraysGenerator {
+    public static HashSet<Class> validType=new HashSet<>();
+    public static final int defaultMaxNum=10000;
+    static {
+        validType.addAll(Arrays.asList(Long.class,Integer.class,Short.class, Double.class,Byte.class));
+    }
+    /**
+     * 随机生成数字类型的数组
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T extends Number> T[] getRandomArray(Class<T> clazz, int size, int maxNums){
+        if(size<=0)
+            throw new RuntimeException("随机生成的数组大小不能为0");
+        ArrayList<T> list=new ArrayList<>();
+        Random random = new Random(new Date().getTime());
+        for(int i=0;i<size;i++){
+            if(!validType.contains(clazz))
+                throw new RuntimeException("不支持的随机生成类型");
+            if(clazz.equals(Long.class)){
+                long l = random.nextLong();
+                list.add((T)Long.valueOf(l));
+            }else if(clazz.equals(Integer.class)){
+                int i1 = random.nextInt(maxNums);
+                list.add((T)Integer.valueOf(i1));
+            }
+        }
+        T[] result = list.toArray((T[])Array
+                .newInstance(clazz.getComponentType(), size)) ;
+        return result;
+    }
+}
